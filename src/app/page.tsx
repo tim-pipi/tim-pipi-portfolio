bbd4a4e82f1c94cea29bf31d032d82800ad572fa
Timothy Putra Prasetio <timothy.prasetio@gmail.com>
2024-12-19 01:14:43 +0800

    Refactor Home component: integrate multiple new sections including Intro, About, Experience, Projects, Skills, Contact, and Footer for a comprehensive layout enhancement.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Header from "@/components/header";
+import Header from '@/components/header';
+import Intro from '@/components/intro';
+import SectionDivider from '@/components/section-divider';
+import About from '@/components/about';
+import Projects from '@/components/projects';
+import Skills from '@/components/skills';
+import Experience from '@/components/experience';
+import Contact from '@/components/contact';
+import Footer from '@/components/footer';
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center px-4">
       <Header />
+      <Intro />
+      <SectionDivider />
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
     </main>
-  )
+  );
 }
